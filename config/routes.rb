0dcd69e43cf5a01ada5ebf79d4351b6abad0089a
James Lieu <j.lieu888@gmail.com>
2013-07-23 10:57:20 +0100

    New User model added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Antisocialnetwork::Application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
 resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
