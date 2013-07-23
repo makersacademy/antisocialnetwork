4956ac84b611b135c7d8d53d3350c06393ed9820
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-07-23 10:38:57 +0100

    stripped out devise

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,9 +31,6 @@ Antisocialnetwork::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # config required by Devise
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 end
