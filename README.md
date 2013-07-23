53446cc3b9fedd5f8d343ea2bcb7db9e5444832c
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-07-23 08:28:15 +0100

    devise login via facebook partially complete

---
+++ README.md
@@ -0,0 +1,26 @@
+README
+======
+
+This repository contains the code for a web application built using Ruby version 1.9.3 and Rails 4.
+
+The application uses a PostgreSQL databases. Run `rake db:create:all` to create the databases followed by `rake db:migrate`.
+
+The application assumes facebook developer credentials are stored in the following environment variables.
+  * `ASN_FACEBOOK_ID` - containing the facebook "app code"
+  * `ASN_FACEBOOK_SECRET` - containing the facebook "app secret code"
+
+
+Tests (Specs) can be run with the `rspec` command or `rake spec` (and its derivatives)
+
+
+
+* Configuration
+
+* Database creation
+
+* Database initialization
+
+* Services (job queues, cache servers, search engines, etc.)
+
+* Deployment instructions
+
---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :rememberable, :trackable, :validatable
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.name = auth.info.name
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"]) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    false
+  end
+
+end
