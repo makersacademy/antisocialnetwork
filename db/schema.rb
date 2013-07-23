ae347bb10fa71fd0b74baffc2de8b7a65b2fa02a
Tom Wisniewski <tom.a.wisniewski@gmail.com>
2013-07-23 10:23:54 +0100

    add customer_id to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130722171653) do
+ActiveRecord::Schema.define(version: 20130723092209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20130722171653) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "encrypted_password"
+    t.string   "customer_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
