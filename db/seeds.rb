ef80ae11a66cc78acf3b25cb03d98e04c0c37e6f
Tom Wisniewski <tom.a.wisniewski@gmail.com>
2013-07-29 11:37:25 +0100

    Minor change

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,22 +13,21 @@ Charity.create( name: "The British Red Cross Society",
                               They are part of a global network of volunteers, responding to natural
                               disasters, conflicts and individual emergencies. They enable vulnerable people
                               at home and overseas to prepare for and respond to emergencies in their own communities.",
-                image: "charity-logo-spam4-A",
-                url: "http://www.redcross.org.uk/")
+                image: "charity-logo-spam4-A.jpg",
+                url: "www.redcross.org.uk/")
 
 Charity.create( name: "Cancer Research UK",
                 registered_number: 1089464,
                 activities: " Cancer Research aims to save lives by preventing, controlling and curing cancer.
                               They do this through funding world-class research into all aspects of cancer, providing
                               information, and influencing public policy.",
-                image: "charity-logo-spam4-B",
-                url: "http://www.cancerresearchuk.org/")
+                image: "charity-logo-spam4-B.jpg",
+                url: "www.cancerresearchuk.org/")
 
 Charity.create( name: "Barnardo's",
                 registered_number: 216250,
                 activities: " Barnardo's aim to promote the care, safety and upbringing of children and young people by
                               supporting and assisting those in need, their families and their carers; promoting their
                               health, and advancing their education.",
-                image: "charity-logo-spam4-C",
-                url: "http://www.barnardos.org.uk/")
-d
+                image: "charity-logo-spam4-C.jpg",
+                url: "www.barnardos.org.uk/")
